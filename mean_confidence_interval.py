c73a022c8d2cc764ae1d84d5ff5b8e934f448116
Teresa <teresa@Teresas-MacBook-Pro.local>
2020-05-08 15:16:55 -0700

    added alternative way of calculating plus minus errorbars

--- mean_confidence_interval.py
+++ mean_confidence_interval.py
@@ -8,4 +8,11 @@ def mean_confidence_interval(data, confidence=0.95):
     h = se * scipy.stats.t.ppf((1 + confidence) / 2., n-1)
     return m, m-h, m+h
 
+def alternative_mean_confidence_interval(data):
 
+    sample_mean = np.mean(data)
+    sample_95ci = 1.96 * scipy.stats.sem(data)
+    # sample_min = sample_mean = sample_95ci
+    # sample_max = sample_mean + sample_95ci
+
+    return sample_mean, sample_95ci
